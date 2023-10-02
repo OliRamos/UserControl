13b239cce07e36c35a635c35260a510a79be5ef3
OliRamos <oli.ramos@ymail.com>
2023-10-02 11:34:32 -0300

    Inserido Cadastro de usuario

--- UsersCase/Controllers/UsuarioController.cs
+++ UsersCase/Controllers/UsuarioController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using UsersCase.Data.Dtos;
+using UsersCase.Models;
 
 namespace UsersCase.Controllers
 {
@@ -8,11 +11,25 @@ namespace UsersCase.Controllers
     [Route("[Controller]")]
     public class UsuarioController : ControllerBase
     {
+        private IMapper _mapper;
+        private UserManager<Usuario> _userManager;
+
+        public UsuarioController(IMapper mapper, UserManager<Usuario> userManager)
+        {
+            _mapper = mapper;
+            _userManager = userManager;
+        }
 
         [HttpPost]
-        public IActionResult CadastraUsuario(CreateUsuarioDto dto)
+        public async Task<IActionResult> CadastraUsuario (CreateUsuarioDto dto)
         {
-            throw new NotImplementedException();
+            Usuario usuario = _mapper.Map<Usuario>(dto);
+
+            IdentityResult resultado = await _userManager.CreateAsync(usuario, dto.Password);
+
+            if (resultado.Succeeded) return Ok("Usuário cadastrado!");
+
+            throw new ApplicationException("Falha ao cadastrar usuário");
         }
     }
 }

f2ec83eed3f58114283bca87a3053a74486962ee
OliRamos <oli.ramos@ymail.com>
2023-10-15 19:04:10 -0300

    inserido secrets para proteção de informacoes sensiveis

--- UsersCase/Program.cs
+++ UsersCase/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using UsersCase.Authorization;
 using UsersCase.Data;
 using UsersCase.Models;
@@ -9,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-var connString = builder.Configuration.GetConnectionString("UsuarioConnection");
+var connString = builder.Configuration["ConnectionStrings:UsuarioConnection"];
 
 builder.Services.AddDbContext<UsuarioDbContext>
     (opts =>
@@ -27,10 +31,27 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<UsuarioService>();
 builder.Services.AddScoped<TokenService>();
 
+builder.Services.AddSingleton<IAuthorizationHandler, IdadeAuthorization>();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["SymmetricSecurityKey"])),
+        ValidateAudience = false, 
+        ValidateIssuer = false,
+        ClockSkew = TimeSpan.Zero
+    };
+});   
 
 builder.Services.AddAuthorization(options =>
     {
@@ -49,6 +70,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
--- UsersCase/Services/TokenService.cs
+++ UsersCase/Services/TokenService.cs
@@ -8,6 +8,13 @@ namespace UsersCase.Services
 {
     public class TokenService
     {
+        private IConfiguration _configuration;
+
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public string GenerateToken(Usuario usuario)
         {
             Claim[] claims = new Claim[]
@@ -18,7 +25,7 @@ namespace UsersCase.Services
                 new Claim("loginTimestamp", DateTime.UtcNow.ToString()),
             };
 
-            var chave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("kljasdo0liklkjsdfgfoip90p2rid0owf90o9ojw"));
+            var chave = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["SymmetricSecurityKey"]));
 
             var signingCredentials = new SigningCredentials(chave, SecurityAlgorithms.HmacSha256);
 
